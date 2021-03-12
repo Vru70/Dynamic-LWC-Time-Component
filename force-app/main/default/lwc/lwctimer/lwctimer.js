3ae175f9c55b33081d0b056d8b5de1db10e87564
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-03-12 17:51:50 +0530

    final changes

--- force-app/main/default/lwc/lwctimer/lwctimer.js
+++ force-app/main/default/lwc/lwctimer/lwctimer.js
@@ -11,42 +11,37 @@ import getOppData from '@salesforce/apex/lwcTimer.getOppData';
 
 export default class Lwctimer extends LightningElement 
 {
-
     @api recordId;
     error;
     jsonData;
-    @track showtimedata =" ";
+    @track exp = false;
+    @track showtimedata = " ";
 
-
-    
     @wire(getOppData, { recId: '$recordId' })
     wiredRecordsMethod({ error, data }) 
     {
-        if(data) 
+        if (data) 
         {
             // getting data
             this.jsonData = data;
             this.error = undefined;
-
             // Slpiting data to get time
-
             var obj = JSON.parse(data);
             var timelong = obj.Expiry_Date__c; //2021-03-11T22:00:00.000+0000         
             var date = timelong.slice(0, -18);
-            console.log('date:', date);
             var time = timelong.slice(11, -5);
-            console.log('time:', time);
             var endtime = date.toString() + ' ' + time.toString();
             var total = Date.parse(endtime) - Date.parse(new Date()); // getting total milisec
-            total = total + 19800000;
-            let myparam = this;
+            total = total + 19800000; // ading 5:30Hr to match IST
+            let myparam = this; // refers this block of context 
             if (total < 0) 
             {
                 window.clearInterval(this.timeInstance);
-                this.showtimedata= 'Expired';
+                this.showtimedata = 'Expired';
+                this.exp = true;
             }
-            else {
-
+            else 
+            {
                 this.timeInstance = setInterval(function () 
                 {
                     var seconds = Math.floor((total / 1000) % 60);
@@ -61,10 +56,10 @@ export default class Lwctimer extends LightningElement
                     var days = Math.floor(total / (1000 * 60 * 60 * 24));
                     // window.console.log('days ' + days);
                     // output the result
-                    var xy = days.toLocaleString() + ' Days : ' + hours.toLocaleString() + ' Hours : ' + minutes.toLocaleString() + ' Minutes : ' + seconds.toLocaleString() + ' Seconds';
-                    
-                    myparam.showtimedata = xy;
-                    window.console.log('this.showtimedata ' + this.showtimedata);
+                    var timeString = days.toLocaleString() + ' Days : ' + hours.toLocaleString() + ' Hours : ' + minutes.toLocaleString() + ' Minutes : ' + seconds.toLocaleString() + ' Seconds';
+
+                    myparam.showtimedata = timeString;
+
                     total = total - 1000;
 
                 }, 1000);
@@ -78,9 +73,5 @@ export default class Lwctimer extends LightningElement
             this.jsonData = undefined;
             console.log('this.jsonData : ', this.jsonData);
         }
-       
-
     }
-    
-
 }
